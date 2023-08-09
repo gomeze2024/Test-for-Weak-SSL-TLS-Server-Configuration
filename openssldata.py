2d46d0a31dd5b497959ffdb1e0ed2b29bf2fa275
gomeze2024 <113722768+gomeze2024@users.noreply.github.com>
2023-08-08 20:48:49 -0400

    Update openssldata.py

--- openssldata.py
+++ openssldata.py
@@ -1,6 +1,7 @@
 import re
 import shlex
 import subprocess
+import platform
 from urllib.parse import urlparse
 
 def validate_url(url):
@@ -14,7 +15,12 @@ def get_ciphers(website_url):
     if not validate_url(website_url):
         raise ValueError("Invalid website URL. Please provide a valid URL starting with http:// or https://")
     try:
-        command = shlex.split("testssl -U -E {}".format(website_url))
+        system = platform.system()
+
+        if system == "Linux":
+            command = shlex.split("testssl -U -E {}".format(website_url))
+        else: 
+            command = shlex.split("testssl.sh -U -E {}".format(website_url))
 
         p1 = subprocess.run(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         stdout_text = p1.stdout
@@ -71,11 +77,15 @@ def nmap_ciphers(website_url):
     website, port = get_website_and_port(website_url)
 
     try:
-        command = shlex.split("nmap -sT -p {} {} --script ssl-enum-ciphers".format(port, website))
+        full_command = "nmap -sT -Pn -p {} {} --script ssl-enum-ciphers".format(port, website)
+        command = shlex.split(full_command)
         p1 = subprocess.run(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         stdout_text = p1.stdout
 
-        return stdout_text, command[0]
+        print(full_command)
+        print(stdout_text)
+        
+        return stdout_text, full_command
     
     except subprocess.CalledProcessError as e:
         print(f"Command returned a non-zero exit status:")
@@ -91,7 +101,7 @@ def parse_url(url):
         return host_with_port, host_without_port
     else:
         raise ValueError("The provided URL is not using the 'https' scheme.")
-    
+
 def get_website_and_netloc(url):
     parsed_url = urlparse(url)
     scheme = parsed_url.scheme
@@ -100,8 +110,15 @@ def get_website_and_netloc(url):
     if scheme != 'http' and scheme != 'https':
         raise ValueError("Invalid URL scheme. Supported schemes are 'http' and 'https'.")
 
-    website = netloc.split(':')[0] if ':' in netloc else netloc
-    return netloc, website
+    default_ports = {'http': 80, 'https': 443}
+    default_port = default_ports[scheme]
+
+    website_parts = netloc.split(':')
+    website = website_parts[0] if website_parts[0] else 'localhost'
+    port = int(website_parts[1]) if len(website_parts) > 1 else default_port
+
+    formatted_website = f"{website}:{port}"
+    return formatted_website, netloc
 
 def openssl_ciphers(website_url, cipher):
     with_port, without_port = get_website_and_netloc(website_url)
@@ -113,19 +130,20 @@ def openssl_ciphers(website_url, cipher):
     try:
         full_command = 'openssl s_client -connect {} -servername {} {} -cipher {}'.format(with_port, without_port, extracted_string, cipher['name'])
         command = shlex.split(full_command)
-        
         print(full_command)
+        p1 = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
 
-        p1 = subprocess.run(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        stdout_text = p1.stdout
+        p1.stdin.write("Q\n")
+        p1.stdin.flush()
+        stdout, stderr = p1.communicate()
 
         pattern = r"(?<=CONNECTED\(00000005\))[\s\S]*?SSL handshake has read([\s\S]*)"
-        match = re.search(pattern, stdout_text)
+        match = re.search(pattern, stdout)
 
         if match:
             extracted_data = "CONNECTED(00000005)\n---\n"+ match.group(1).strip()
             print(extracted_data)
-            return extracted_data, command
+            return extracted_data, full_command
         
         else:
             print("Could not establish connection")
